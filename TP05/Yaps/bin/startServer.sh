1c221e3c4e11f32bd209449d68bded602f1ff1ee
任俊州, JunZhou REN <renjunzhou@emails.bjut.edu.cn>
2016-11-15 20:57:40 +0100

    TP05
    RMI 
    Original

---
+++ TP05/Yaps/bin/startServer.sh
@@ -0,0 +1,24 @@
+
+cygwin=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+esac
+
+JAVA=$JAVA_HOME/bin/java
+RMIREGISTRY=$JAVA_HOME/bin/rmiregistry
+
+DEPLOY_DIR=../build
+LIB_DIR=../lib
+
+CLASSPATH=$DEPLOY_DIR/server.jar:$DEPLOY_DIR/common.jar:$MYSQL_HOME/lib/mysql-connector-java-5.1.21-bin.jar
+
+if $cygwin; then
+# CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+CLASSPATH="$DEPLOY_DIR/server.jar;$DEPLOY_DIR/common.jar;$MYSQL_HOME/lib/mysql-connector-java-5.1.21-bin.jar"
+fi
+
+
+$RMIREGISTRY &
+
+$JAVA -classpath "$CLASSPATH" com.yaps.petstore.server.RegisterServices
+
