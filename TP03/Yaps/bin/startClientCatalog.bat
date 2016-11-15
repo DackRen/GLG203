462ef33bd92197dbf5f4b22ff20830d5c2e0cc66
JunZhou REN <dackren@icloud.com>
2016-11-15 21:39:52 +0100

    still whatever

--- TP03/Yaps/bin/startClientCatalog.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set JAVA=%JAVA_HOME%\bin\java
-set DEPLOY_DIR=..\build
-
-set CLASSPATH=%DEPLOY_DIR%\clientCatalog.jar;%MYSQL_HOME%\lib\mysql-connector-java-5.1.21-bin.jar
-
-%JAVA% -cp %CLASSPATH% com.yaps.petstore.ui.MenuCatalog
--- TP04/Yaps/bin/startClientCustomer.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-set JAVA=%JAVA_HOME%\bin\java
-set DEPLOY_DIR=..\build
-
-set CLASSPATH=%DEPLOY_DIR%\clientCustomer.jar;%MYSQL_HOME%\lib\mysql-connector-java-5.1.21-bin.jar
-
-rem start the application with a specific logger defined in mylogging.properties
-%JAVA% -cp %CLASSPATH% -Djava.util.logging.config.file=mylogging.properties com.yaps.petstore.ui.text.MenuCustomer
